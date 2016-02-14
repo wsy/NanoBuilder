93b4ee60f678c82ef870365c44b80c1a5d737ee7
Flynn Bundy <f.bundy@coolblue.nl>
2016-02-14 19:07:25 +0100

    adding simple pester test - more to be added

---
+++ NanoCreator.Tests.ps1
@@ -0,0 +1,9 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+Describe "NanoCreator" {
+    It "ISO mounted" {
+        Get-WmiObject -Class WIN32_LogicalDisk | Where {$_.DeviceID -eq 5}  | Should Be $True
+    }
+}
