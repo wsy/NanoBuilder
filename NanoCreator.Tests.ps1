9dca2b24a9df3832a45e4ef5016231e55d741867
Flynn Bundy <flynn.sbundy@gmail.com>
2016-03-14 18:52:00 +0100

    Delete NanoCreator.Tests.ps1

--- NanoCreator.Tests.ps1
+++
@@ -1,9 +0,0 @@
-﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
-. "$here\$sut"
-
-Describe "NanoCreator" {
-    It "ISO mounted" {
-        Get-WmiObject -Class WIN32_LogicalDisk | Where {$_.DeviceID -eq 5}  | Should Be $True
-    }
-}
